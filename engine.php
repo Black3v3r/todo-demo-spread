1a7f5f213552b70f9b292e4a022bbdd36e7bb20a
Black3v3r <black3v3r@gmail.com>
2017-03-25 00:06:44 +0100

    Start to work on deletions

--- engine.php
+++ engine.php
@@ -5,11 +5,19 @@ require_once 'error_handling.php';
 require_once 'db_config.inc.php';
 require_once 'Database.php';
 
-if (isset($_POST) && isset($_POST['item-title'])) {
+var_dump($_POST);
+
+if (isset($_GET['action']) && ($_GET['action'] == 'add') && isset($_POST['item-title']))
+{
     $db = new Database($dbConfig);
     $db->insert('items', [
         'title' => htmlspecialchars($_POST['item-title'])
     ]);
 }
 
-header('Location: ./');
+elseif (isset($_GET['action']) && ($_GET['action'] == 'delete'))
+{
+    var_dump($_POST);
+}
+
+//header('Location: ./');
