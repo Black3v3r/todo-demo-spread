1f35edd8e1c8f23bb4350716f7ed38b112fc13c8
Black3v3r <black3v3r@gmail.com>
2017-03-25 11:54:46 +0100

    Populate database (for testing purposes)

---
+++ fill_db.sql
@@ -0,0 +1,6 @@
+INSERT INTO todolist.items(title) VALUES ('TODO 01');
+INSERT INTO todolist.items(title) VALUES ('TODO 02');
+INSERT INTO todolist.items(title) VALUES ('TODO 03');
+INSERT INTO todolist.items(title) VALUES ('TODO 04');
+INSERT INTO todolist.items(title) VALUES ('TODO 05');
+INSERT INTO todolist.items(title) VALUES ('TODO 06');
